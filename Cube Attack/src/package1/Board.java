6d948a94fd0c798aec40e22dfc63862670df2d5a
Spiggoingio <jspig0@gmail.com>
2015-06-07 16:45:42 -0700

    Draws a block!

---
+++ Cube Attack/src/package1/Board.java
@@ -0,0 +1,52 @@
+
+package package1;
+import java.applet.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Board extends Applet {
+   
+    public Board()
+    {
+        Block[][] blockArray = new Block[5][5];
+        JFrame window = buildFrame();
+       
+        try {
+            final BufferedImage image = ImageIO.read(new File("src\\resources\\block.png"));
+            JPanel pane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+            window.add(pane);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Board.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        
+    }
+    public JFrame buildFrame(){
+        JFrame window = new JFrame("Cube Attack");
+        JLabel label = new JLabel("BERTOS GAY",SwingConstants.CENTER);
+        label.setPreferredSize(new Dimension(300,100)); 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        window.setPreferredSize(new Dimension (500,750));
+        window.getContentPane().add(label,BorderLayout.CENTER);
+        window.setLocationRelativeTo(null);
+        window.pack();
+        window.setVisible(true);
+        return window;
+    }
+}
