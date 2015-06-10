c9c7e6d360e5b7601389f6ab9279ae1ed7ad113b
albertoarmienta <albertoarmienta@gmail.com>
2015-06-09 19:03:37 -0700

    Blocks now fall down appropriately.

--- Cube Attack/src/package1/Cursor.java
+++ Cube Attack/src/package1/Cursor.java
@@ -5,53 +5,71 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Cursor {
-    private int target1x;
-    private int targety;
-    private int target2x;
+    private int cursor_x_left;
+    private int cursor_x_right;
+    private int cursor_y;
     private ImageIcon imageIcon;
     private Image image;
     public Cursor(){
         imageIcon = new ImageIcon("src/resources/cursor.png");
         image = imageIcon.getImage();
-        target1x = 0;
-        target2x = 1;
-        targety = 0;
+        cursor_x_left = 0;
+        cursor_x_right = 1;
+        cursor_y = Board.MAX_Y - 1;
     }
     public int getTarget1x(){
-        return target1x;
+        return cursor_x_left;
     }
     public int getTarget2x(){
-        return target2x;
+        return cursor_x_right;
     }
     public int getTargety(){
-        return targety;
+        return cursor_y;
     }
     public void moveLeft(){
-        if(target1x>0){
-            target1x--;
-            target2x--;
+        if(cursor_x_left>0){
+            cursor_x_left--;
+            cursor_x_right--;
         }
     }
     public void moveRight(){
-        if(target2x<Board.MAX_X-1){
-            target1x++;
-            target2x++;
+        if(cursor_x_right<Board.MAX_X-1){
+            cursor_x_left++;
+            cursor_x_right++;
         }
     }
     public void moveUp(){
-        if(targety>0){
-            targety--;
+        if(cursor_y>0){
+            cursor_y--;
         }
     }
     public void moveDown(){
-        if(targety<Board.MAX_Y-1){
-            targety++;
+        if(cursor_y<Board.MAX_Y-1){
+            cursor_y++;
         }
     }
     public void swapTargets(){
-        Block temp = Board.levelArray[target1x][targety];
-        Board.levelArray[target1x][targety] = Board.levelArray[target2x][targety];
-        Board.levelArray[target2x][targety] = temp;
+        Block temp = Board.levelArray[cursor_x_left][cursor_y];
+        Board.levelArray[cursor_x_left][cursor_y] = Board.levelArray[cursor_x_right][cursor_y];
+        Board.levelArray[cursor_x_right][cursor_y] = temp;
+
+				/*Count is one because you only moved horizontal*/
+				if(Board.levelArray[cursor_x_right][cursor_y].color == "EMPTY" )
+					Board.fallingBlocks(cursor_x_right, cursor_y, 1);
+				else if(Board.levelArray[cursor_x_left][cursor_y].color == "EMPTY" )
+					Board.fallingBlocks(cursor_x_left, cursor_y, 1);
+
+				if(Board.levelArray[cursor_x_right][cursor_y + 1].color == "EMPTY")
+				{
+					System.out.println("tits right");
+					Board.fallingBlocks(cursor_x_right, cursor_y, 1);
+				}
+				else if(Board.levelArray[cursor_x_left][cursor_y + 1].color == "EMPTY")
+				{
+					System.out.println("tits left");
+					Board.fallingBlocks(cursor_x_left, cursor_y, 1);
+				}
+					
     }
     public Image getImage(){
         return image;
