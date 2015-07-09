b58c9d1146e64110351aacb193606709e417687e
Spiggoingio <jspig0@gmail.com>
2015-07-08 21:32:59 -0700

    "To Do List" added to resources. Some additions to GUIPanel.  [gameState ; repaint() ; menu ]

--- Cube Attack/src/package1/GUIPanel.java
+++ Cube Attack/src/package1/GUIPanel.java
@@ -13,14 +13,33 @@ import javax.swing.JLabel;
 
 class GUIPanel extends Applet implements ActionListener{
     public final int WIDTH = 1005, HEIGHT = 829;
+    public int gameState = 0;
     public Board b1;
     public Board b2;
     public JLabel banner;
+    private JLabel menu;
+    private JFrame game = buildFrame();
     public GUIPanel() 
     {
-        JFrame game = buildFrame();
+        menu = new JLabel();
+        menu.setBounds(0,0,WIDTH,HEIGHT);
+        menu.setIcon(new ImageIcon("src/resources/MENU.png"));
+        game.getContentPane().add(menu);
         setFocusable(true);
         game.addKeyListener(new TAdapter());
+        game.setVisible(true);
+    }
+    public JFrame buildFrame() {
+        JFrame window = new JFrame("Cube Attack");
+        window.setLayout(null);
+        window.setPreferredSize(new Dimension( WIDTH, HEIGHT));
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        return window;
+    }
+    public void startGame(){
+        game.remove(menu);
         b1 = new Board();
         b1.setBounds(0,0,b1.WIDTH,b1.HEIGHT);
         b2 = new Board();
@@ -28,30 +47,16 @@ class GUIPanel extends Applet implements ActionListener{
         banner = new JLabel();
         banner.setBounds(b1.WIDTH, 0, WIDTH-b1.WIDTH-b2.WIDTH,b1.HEIGHT);
         banner.setIcon(new ImageIcon("src/resources/BANNER.png"));
-        System.out.println(banner.getBounds());
         game.getContentPane().add(banner);
         game.getContentPane().add(b1);
         game.getContentPane().add(b2);
-        game.setVisible(true);
-    }
-    public JFrame buildFrame() {
-        JFrame window = new JFrame("Cube Attack");
-        window.setLayout(null);
+        game.repaint();
         
-       // System.out.println(window.getLayout());
-        window.setPreferredSize(new Dimension( WIDTH, HEIGHT));
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //window.setLocationRelativeTo(null);
-        
-        window.pack();
-        //window.setVisible(true);
-        return window;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
  private class TAdapter extends KeyAdapter {
@@ -64,28 +69,50 @@ class GUIPanel extends Applet implements ActionListener{
             int key = e.getKeyCode();
             switch(key){
                 case KeyEvent.VK_ENTER:
+                    if(gameState == 1){
                     b1.moveUp();
                     b2.moveUp();
+                    }
+                    else if(gameState==0)
+                    {
+                        startGame();
+                        gameState = 1;
+                    }
                     break;
                 case KeyEvent.VK_SPACE:
+                    if(gameState == 1){
                     b2.swapTargets();
                     b2.adjacencyCheck( b2.levelCursor.getCursorx(),  b2.levelCursor.getCursory());
                     b2.adjacencyCheck( b2.levelCursor.getCursor2x(),  b2.levelCursor.getCursory());
+                    }
                     break;
                 case KeyEvent.VK_UP:
+                    if(gameState == 1){
                      b2.levelCursor.moveUp();
+                    }
                     break;
                 case KeyEvent.VK_DOWN:
+                    if(gameState == 1){
                      b2.levelCursor.moveDown();
+                    }
                     break;
                 case KeyEvent.VK_LEFT:
+                    if(gameState == 1){
                      b2.levelCursor.moveLeft();
+                    }
                     break;
                 case KeyEvent.VK_RIGHT:
+                    if(gameState == 1){
                      b2.levelCursor.moveRight();
+                    }
                     break;
                 case KeyEvent.VK_0:
-                    b2.shiftDown();
+                    if(gameState==1)
+                    {
+                        //game.dispose();
+                        //gameState = 0;
+                    }
+                    break;
             }
             
         }
--- Cube Attack/src/package1/Main.java
+++ Cube Attack/src/package1/Main.java
@@ -1,6 +1,6 @@
 /*
  * Cube Attack
- * v0.0.3
+ * v0.0.4
  * Spigno/Armienta
  */
 
